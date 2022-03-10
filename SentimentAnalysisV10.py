37db4a2c65b827ee499ce4e678b3c6ebb9c07d4c
TobiasRothlin <tobias@rothlin.com>
2022-03-10 09:33:15 +0100

    ModelReport Added

---
+++ SentimentAnalysisV10.py
@@ -0,0 +1,9 @@
+
+class HotelSentimentAnalysis:
+
+    def __init__(self,trainingData):
+        pass
+
+
+    def getSentiment(self,text):
+        return 'Positiv',1
---
+++ test_ModelReport.py
@@ -0,0 +1,46 @@
+import unittest
+from ModelReport import ModelReport
+from DataHandler.DataHandler import DataHandler
+import random
+
+class Test_ModelReport(unittest.TestCase):
+    def test_CreateReport(self):
+        modelName = "TestModel"
+        modelCreator = "Tobias Rothlin"
+        mlPrinciple = "Naive Bayes"
+        refrences = {'Wikipedia':"https://en.wikipedia.org/wiki/Naive_Bayes_classifier","Scikit-learn":"https://scikit-learn.org/stable/modules/naive_bayes.html"}
+        algorithemDescription = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id leo eu enim tempus rhoncus. Pellentesque fringilla at mi sit amet rutrum. Donec vehicula metus urna, maximus eleifend nibh consequat in. Mauris nunc ipsum, tristique non ligula id, tempor luctus turpis. Sed vehicula, ex id eleifend accumsan, augue nisi egestas mi, vel pretium justo magna quis erat. Donec et felis felis. Maecenas ut nunc ut urna sodales scelerisque. Sed sollicitudin facilisis sapien pharetra molestie. Donec accumsan nunc et dui aliquam, eget egestas ligula ullamcorper. Proin vitae tincidunt dolor. Vivamus imperdiet et velit vel laoreet. Donec nisi sem, rhoncus a orci id, ornare efficitur turpis. Mauris vitae ligula arcu. Aliquam felis neque, laoreet nec ante eu, dignissim tristique mi. Vivamus vitae lacus eu diam ultricies finibus at at libero."""
+        graphicPath ="/Users/tobiasrothlin/Documents/BachelorArbeit/SentimentAnalysis/ROC_curves.png"
+        graphicDescription = "The ROC curve of a naie Bayes Classifiere"
+
+        #Using all Data as TrainingData
+        myDatahandler = DataHandler()
+        trainingSet = myDatahandler.getCategorieData("Location")
+
+        #Creating the test Results
+        mappingTable = {
+            1: "Location",
+            2: "Room",
+            3: "Food",
+            4: "Staff",
+            5: "ReasonForStay",
+            6: "GeneralUtility",
+            7: "HotelOrganisation",
+            8: "Unknown"
+        }
+        testResults = []
+
+        for i in range(100 ):
+            true = random.randint(1,8)
+            predicted = random.randint(1,8)
+            testResults.append([mappingTable[true],mappingTable[predicted]])
+
+        myModelReport = ModelReport(modelName,modelCreator,mlPrinciple,refrences,algorithemDescription,graphicPath,graphicDescription)
+        myModelReport.addTrainingSet(trainingSet)
+        myModelReport.addTestResults(testResults)
+        myModelReport.createRaport()
+
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
